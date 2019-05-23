b61861bce9fc3b305d4714acdd881fe4ce2da72d
CRIMX <straybugs@gmail.com>
2019-05-23 18:47:54 +0800

    replace img with responsive b-img

--- components/sections/Features.vue
+++ components/sections/Features.vue
@@ -2,31 +2,31 @@
   <section>
     <feature desc="search" />
     <div class="text-center px-4">
-      <img
-        class="mw-100"
-        src="https://raw.githubusercontent.com/wiki/crimx/ext-saladict/images/pin.gif"
+      <b-img
+        fluid
         width="789"
         height="646"
+        src="https://raw.githubusercontent.com/wiki/crimx/ext-saladict/images/pin.gif"
         alt="Saladict Pin"
       />
     </div>
     <feature desc="notebook" />
     <div class="text-center px-4">
-      <img
-        class="mw-100"
-        src="https://raw.githubusercontent.com/wiki/crimx/ext-saladict/images/screen-notebook.png"
+      <b-img
+        fluid
         width="1344"
         height="445"
+        src="https://raw.githubusercontent.com/wiki/crimx/ext-saladict/images/screen-notebook.png"
         alt="Saladict Notebook"
       />
     </div>
     <feature desc="langs" />
     <div class="text-center px-4">
-      <img
-        class="mw-100"
-        src="https://raw.githubusercontent.com/wiki/crimx/ext-saladict/images/youdao-page.gif"
+      <b-img
+        fluid
         width="1034"
         height="646"
+        src="https://raw.githubusercontent.com/wiki/crimx/ext-saladict/images/youdao-page.gif"
         alt="Saladict Youdao Page Translate"
       />
     </div>
