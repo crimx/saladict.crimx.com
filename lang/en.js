3393c52bdef54d4abfc7173ae45d49a6a801928a
CRIMX <straybugs@gmail.com>
2019-05-23 18:48:09 +0800

    split English text

--- lang/en.js
+++ lang/en.js
@@ -1,7 +1,7 @@
 export default {
   title: 'Saladict',
   description:
-    'Saladict is professional inline translator browser extension, built for comparative reading, with comprehensive dictionaries covering all major languages.',
+    'Saladict is a professional inline translator browser extension, built for comparative reading, with comprehensive dictionaries covering all major languages.',
   cws: 'Chrome Web Store',
   amo: 'Add-ons for Firefox',
   dd: 'Direct Download',
@@ -11,8 +11,10 @@ export default {
     search: {
       title: 'Looking up words has never been easier',
       content: [
-        'Saladict supports mixed use of multiple search modes. Whether it be single-click, double-click, pop-up icon, hovering or keyboard shortcuts, you name it.',
-        'Multiple dictionaries will be searching at the same time whose combination is adjusted dynamically according to the language and length of the search text and Saladict profile.'
+        'Saladict supports mixed use of multiple search modes.',
+        'Whether it be single-click, double-click, pop-up icon, hovering or keyboard shortcuts, you name it.',
+        'Multiple dictionaries will be searching at the same time.',
+        'The combination of result is adjusted dynamically according to the language and length of the search text and Saladict profile.'
       ]
     },
     notebook: {
@@ -20,21 +22,23 @@ export default {
       content: [
         'Saladict comes with search history and a vocabulary notebook.',
         'Context of each word will also be captured and translated.',
-        'Notebook can be synced cross devices via WebDAV, or be exported to other apps for memorization.'
+        'Notebook can be synced across devices via WebDAV, or be exported to other apps for memorization.'
       ]
     },
     langs: {
       title: 'Multilingual support, more than you expect',
       content: [
         'Although Saladict is built for looking up English words, it also supports other languages.',
-        'Chinese (simplified and traditional), Japanese, Korean, French, German and Spanish are supported with dictionaries. Other languages are supported with several machine translation services.',
+        'Chinese (simplified and traditional), Japanese, Korean, French, German and Spanish are supported with dictionaries.',
+        'Other languages are supported with several machine translation services.',
         'Each dictionary is well polished to a unify style for comfortable reading experience. (You should send love to the author.)'
       ]
     },
     paper: {
       title: 'Reading foreign essay is a piece of "paper"',
       content: [
-        'Saladict has three default profiles: Scholar Mode, Translation Mode and Sentence Mode, which can be used for reading, translating and writing essays in foreign language.',
+        'Saladict has three default profiles: Scholar Mode, Translation Mode and Sentence Mode.',
+        'These may come in handy when reading, translating and writing essays in foreign language.',
         'There is also a build-in PDF viewer which can be used in conjunction with sidebar-mode standalone search panel.'
       ]
     }
