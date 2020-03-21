595186c67e1168a81c6b5883a83d25490900d81b
crimx <straybugs@gmail.com>
2020-03-21 14:06:08 +0800

    fix: vue-google-adsense ssr error

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,9 +1,15 @@
-import Adsense from 'vue-google-adsense/dist/Adsense.min.js'
-import InArticleAdsense from 'vue-google-adsense/dist/InArticleAdsense.min.js'
-
 export default ({ Vue, options, router, siteData }) => {
-  Vue.use(require('vue-script2'))
-
-  Vue.use(Adsense)
-  Vue.use(InArticleAdsense)
+  if (typeof window !== 'undefined') {
+    import('vue-google-adsense')
+      .then(module => {
+        const Ads = module.default
+        Vue.use(require('vue-script2'))
+        Vue.use(Ads.Adsense)
+        Vue.use(Ads.InArticleAdsense)
+        Vue.use(Ads.InFeedAdsense)
+      })
+      .catch(e => {
+        console.log(e)
+      })
+  }
 }
