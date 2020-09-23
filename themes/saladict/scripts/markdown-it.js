c618d6d5a1f6e18731f40e28c3856d56a617f590
crimx <straybugs@gmail.com>
2020-09-23 21:20:01 +0800

    refactor: add article styles

---
+++ themes/saladict/scripts/markdown-it.js
@@ -0,0 +1,10 @@
+hexo.extend.filter.register('markdown-it:renderer', function (md) {
+  md.use(require('markdown-it-anchor'), {
+    level: 1,
+    permalink: true,
+    permalinkBefore: true,
+    permalinkClass: 'article-header-anchor',
+  }).use(require('markdown-it-attrs'), {
+    allowedAttributes: ['id'],
+  })
+})
