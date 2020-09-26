fd8d9a450a1cf363f5d8623553e4e62fcd59cb9e
crimx <straybugs@gmail.com>
2020-09-26 18:06:31 +0800

    refactor: add releases/all

--- themes/saladict/scripts/releases.js
+++ themes/saladict/scripts/releases.js
@@ -1,11 +1,12 @@
 const JSDOM = require('jsdom').JSDOM
 const _ = require('lodash')
 
-hexo.extend.generator.register('releases', (locals) => {
+hexo.extend.generator.register('releases', function (locals) {
   const sortedReleases = locals.pages
     .filter((page) => page.path.startsWith('releases/'))
     .map((page) => ({
       page,
+      dom: new JSDOM(page.content),
       value: Number(
         page.path
           .slice(10, -5)
@@ -16,19 +17,50 @@ hexo.extend.generator.register('releases', (locals) => {
     }))
     .sort((a, b) => b.value - a.value)
 
-  const latestRelease = sortedReleases[0].page
+  const allReleases =
+    '<h1>Releases</h1>\n' +
+    sortedReleases
+      .map((release) => {
+        const title = (/^releases\/(.+)\.html$/.exec(release.page.path) || [
+          '',
+          '',
+        ])[1]
+        const content = [...release.dom.window.document.querySelectorAll('h2')]
+          .map((h2) => {
+            const root = this.config.root || '/'
+            const href = `${root}${release.page.path}#${h2.id}`
+            return `<p><a href="${href}">${getHeaderText(h2)}</a></p>`
+          })
+          .join('\n')
+        return `<h2>${title}</h2>\n${content}`
+      })
+      .join('\n')
 
-  const dom = new JSDOM(latestRelease.content)
   const [chs, eng] = _.unzip(
-    [...dom.window.document.querySelectorAll('li')].map((li) =>
-      li.textContent.split('\n').map((line) => line.trim())
-    )
+    [
+      ...sortedReleases[0].dom.window.document.querySelectorAll('li'),
+    ].map((li) => li.textContent.split('\n').map((line) => line.trim()))
   )
-  const version = dom.window.document.querySelector('h2').textContent.trim()
+  const version = getHeaderText(
+    sortedReleases[0].dom.window.document.querySelector('h2')
+  )
 
   return [
-    { path: 'releases/index.html', data: latestRelease, layout: 'page' },
+    {
+      path: 'releases/all.html',
+      data: { content: allReleases },
+      layout: 'page',
+    },
+    {
+      path: 'releases/index.html',
+      data: sortedReleases[0].page,
+      layout: 'page',
+    },
     { path: 'releases/chs.json', data: JSON.stringify({ version, data: chs }) },
     { path: 'releases/eng.json', data: JSON.stringify({ version, data: eng }) },
   ]
 })
+
+function getHeaderText(el) {
+  return (el.textContent || '').replace(/¶/, '').trim()
+}
